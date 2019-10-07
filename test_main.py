cc9db7e00b51c8deade22f9b68ed732c14550812
Carlos Rojas <carlos.rojas@sjsu.edu>
2019-10-07 01:08:48 -0700

    Fix bug

--- test_main.py
+++ test_main.py
@@ -4,5 +4,4 @@ def test_main():
     # set up test
     a = [1,2,3]
     b = [4,5]
-    assert main.concat_list(a,b) == [1,2,3,4,5]
-    assert False
+    assert main.concat_lists(a,b) == [1,2,3,4,5]
